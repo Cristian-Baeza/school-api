ff0cba06f6e17726773cfd124a99115663fd8542
Cristian-Baeza <cristianbaeza092@gmail.com>
2021-04-08 19:38:51 -0600

    Heroku Setup

--- school_api/settings.py
+++ school_api/settings.py
@@ -1,4 +1,4 @@
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,3 +113,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
